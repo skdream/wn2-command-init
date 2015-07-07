1a9ea535696e64fcde735c3d6db79f5bdeafed58
jiyj <jiyongjia@gmail.com>
2015-07-07 18:43:22 +0800

    index

--- index.js
+++ index.js
@@ -18,9 +18,43 @@ var path = require('path');
 
 
 exports.register = function(commander){
+
+
+	 commander
+        .option('-v, --verbose', 'output verbose help', Boolean, false);
+
+
+	commander.on('--help', function(){
+	  console.log('  Examples:');
+	  console.log('');
+	  console.log('    $ init site');
+	  console.log('    $ init m-site');
+  	  console.log('    $ init special');
+	  console.log('    $ init m-special');
+	  console.log('');
+	});
+
+
+    commander.action(function () {
+        var args = Array.prototype.slice.call(arguments);
+        var options = args.pop();
+        var template = args.shift();
+
+        if (options.verbose) {
+            fis.log.level = fis.log.L_ALL;
+            fis.log.throw = true;
+        }
+
+        if (!template) {
+            commander.outputHelp();
+            return;
+        }
+    });
+
+
 	fis.util.map(templates,function(index, elem) {
 
-		console.log(index);
+		//console.log(index);
 	})
 }
 
